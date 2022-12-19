f8900ae716469e939e941da7d2aec9eaf81eabe9
Eric Waithaka <74556293+eric-waithaka@users.noreply.github.com>
2022-12-19 05:45:56 +0300

    Removed commented out code

--- script.js
+++ script.js
@@ -3,10 +3,8 @@ const questions = document.querySelectorAll(".question");
 
 questions.forEach(function (question) {
   const btn = question.querySelector(".question-btn");
-  // console.log(btn);
 
   btn.addEventListener("click", function () {
-    // console.log(question);
 
     questions.forEach(function (item) {
       if (item !== question) {
@@ -17,14 +15,3 @@ questions.forEach(function (question) {
     question.classList.toggle("show-text");
   });
 });
-
-// traversing the dom
-// const btns = document.querySelectorAll(".question-btn");
-
-// btns.forEach(function (btn) {
-//   btn.addEventListener("click", function (e) {
-//     const question = e.currentTarget.parentElement.parentElement;
-
-//     question.classList.toggle("show-text");
-//   });
-// });
